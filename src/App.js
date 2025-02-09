f51032e82a9acbb80c586595c8670a603e6dd68a
Red Chang <redmabangis@gmail.com>
2025-02-09 13:11:32 -0500

    Added everything

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>101461871</h1>
+      <h1>Ralph Edward Chang</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Ralph Edward Chang
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
